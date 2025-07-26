fb3fbd99d01e5e0ab93081638eccf4d9d900ccb3
pj46280 <priyanshjha.pj@gmail.com>
2025-07-27 00:11:53 +0530

    updated

--- 13RomanToInteger.py
+++ 13RomanToInteger.py
@@ -10,20 +10,26 @@ class Solution:
             'D': 500,
             'M': 1000
         }
-        i=0
         n=0
-        while True:
-            if i == len(s) - 1:
-                a, b = s[i-1], s[i]
-                if hashMap[a] >= hashMap[b]:
-                    n += hashMap[b]
-                break
-            a, b = s[i], s[i+1]
-            if hashMap[b] > hashMap[a]:
-                n += hashMap[b] - hashMap[a]
+        for i in range(len(s)):
+            if (i+1) < len(s) and hashMap[s[i]] < hashMap[s[i+1]]:
+                n -= hashMap[s[i]]
             else:
-                n += hashMap[a]
-            i+=1
+                n += hashMap[s[i]]
+
+        # while True:
+        #     if i == len(s) - 1:
+        #         a, b = s[i-1], s[i]
+        #         if hashMap[a] >= hashMap[b]:
+        #             n += hashMap[b]
+        #         break
+        #     a, b = s[i], s[i+1]
+        #     if hashMap[b] > hashMap[a]:
+        #         n += hashMap[b] - hashMap[a]
+        #     else:
+        #         n += hashMap[a]
+        #     print(n)
+        #     i+=1
 
         return n
 
