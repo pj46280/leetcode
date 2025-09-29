e2648c2fbf6ef20ceb5d5d4c7eb8694f4efcdca3
pj46280 <priyanshjha.pj@gmail.com>
2025-09-30 00:01:20 +0530

    updated

---
+++ 91DecodeWays.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        d = {len(s): 1}
+
+        def backtrack(i):
+            if i in d:
+                return d[i]
+            if s[i] == "0":
+                return 0
+            res = backtrack(i+1)
+            if (i+1) < len(s) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456"):
+                res += backtrack(i+2)
+            d[i] = res
+            return res
+
+        return backtrack(0)
+        
