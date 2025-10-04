d2b937d08e633dc5207b3bab120886ab93e1c39a
pj46280 <priyanshjha.pj@gmail.com>
2025-10-04 23:49:37 +0530

    updated

--- 148SortList.py
+++ 148SortList.py
@@ -48,3 +48,83 @@ class Solution:
 
         return merge_sort(head)
 
+
+
+## Constant Space Solution
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def merge(l, r):
+            dummy = ListNode(0)
+            tail = dummy
+            while l and r:
+                if l.val < r.val:
+                    tail.next = l
+                    l = l.next
+                else:
+                    tail.next = r
+                    r = r.next
+                tail = tail.next
+            
+            while l:
+                tail.next = l
+                l = l.next
+                tail = tail.next
+
+            while r:
+                tail.next = r
+                r = r.next
+                tail = tail.next
+
+            return dummy.next, tail
+
+        def split(head, step):
+            for _ in range(step - 1):
+                if not head:
+                    break
+                head = head.next
+
+            if not head:
+                return None
+
+            second = head.next
+            head.next = None
+            return second
+
+        def merge_sort(head):
+            if not head or not head.next:
+                return head
+            
+            length = 0
+            node = head
+            while node:
+                length += 1
+                node = node.next
+
+            dummy = ListNode(0)
+            dummy.next = head
+
+            step = 1
+
+            while step < length:
+                pre, cur = dummy, dummy.next
+                while cur:
+                    left = cur
+                    right = split(left, step)
+                    cur = split(right, step)
+
+                    merged_head, merged_tail = merge(left, right)
+                    pre.next = merged_head
+                    pre = merged_tail
+
+                step *= 2
+            
+            return dummy.next
+
+        return merge_sort(head)
+
