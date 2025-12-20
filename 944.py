a317e3df7dab4094f800c077dff9cacd8d40f31d
pj46280 <priyanshjha.pj@gmail.com>
2025-12-20 10:35:52 +0530

    +=944

---
+++ 944.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        rows = len(strs)
+        cols = len(strs[0])
+        count = 0
+
+        for j in range(cols):
+            for i in range(1, rows):
+                if strs[i][j] < strs[i-1][j]:
+                    count += 1
+                    break
+
+        return count
+
