50fd16dbc0942491d12feacdbaea7d374ea026ab
pj46280 <priyanshjha.pj@gmail.com>
2025-12-26 19:37:30 +0530

    +=2483

---
+++ 2483.py
@@ -0,0 +1,25 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        n = len(customers)
+        prefix_n = [0] * (n+1)
+        postfix_y = [0] * (n+1)
+
+        for i in range(1, n+1):
+            prefix_n[i] = prefix_n[i-1]
+            if customers[i-1] == 'N': 
+                prefix_n[i] += 1
+        
+        for i in range(n-1, -1, -1):
+            postfix_y[i] = postfix_y[i+1]
+            if customers[i] == 'Y': 
+                postfix_y[i] += 1
+
+        min_p, idx = float("inf"), 0
+        for i in range(n+1):
+            p = prefix_n[i] + postfix_y[i]
+            if p < min_p:
+                min_p = p
+                idx = i
+
+        return idx
+
