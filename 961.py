0549cc6e438a0b62ea1e27b9a0296e3cc64aa465
pj46280 <priyanshjha.pj@gmail.com>
2026-01-02 20:44:48 +0530

    +=961

---
+++ 961.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        for k in range(1, 4):
+            for i in range(len(nums) - k):
+                if nums[i] == nums[i+k]:
+                    return nums[i]
+                    
+
